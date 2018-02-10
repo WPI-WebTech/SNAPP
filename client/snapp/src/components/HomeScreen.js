bf3539fe0eb527db4638584bae486f3e94ccf4fb
fegan104 <fegan104@gmail.com>
2018-02-10 10:56:43 -0500

    minor ui improvements

--- client/snapp/src/components/HomeScreen.js
+++ client/snapp/src/components/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { requestRide, cancelRide } from "../actions/RideActions";
 import { rideStatus } from "../constants";
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 /**
  * Screen with map and form where users can request rides
@@ -20,54 +22,64 @@ class HomeScreen extends Component {
     }
 
     // validate user input and request a ride
-    requestRide() {
+    requestRide = () => {
         const { fromAddress, toAddress, numPassengers, accommodations } = this.state;
         const numRiders = parseInt(numPassengers, 10);
         if (fromAddress && toAddress && numRiders > 0 && numRiders < 6) {
-            this.props.requestRide({ fromAddress, toAddress, numPassengers: numRiders, accommodations});
+            this.props.requestRide({ fromAddress, toAddress, numPassengers: numRiders, accommodations });
         }
     }
 
     // render page content depending on ride status state
     renderPageContent() {
-        switch(this.props.rideStatus) {
+        switch (this.props.rideStatus) {
             case rideStatus.DEFAULT:
                 return (
                     <div className="Request-form">
                         <p className="Request-title">Request a Ride</p>
                         <div className="Form-row">
-                            <label className="Form-label">From</label>
-                            <input
+                            <TextField
+                                fullWidth={true}
                                 className="Form-text-input"
-                                type="text" placeholder="Address"
+                                type="text"
+                                placeholder="Pickup address"
                                 value={this.state.fromAddress}
                                 onChange={(event) => this.setState({ fromAddress: event.target.value })} />
                         </div>
                         <div className="Form-row">
-                            <label className="Form-label">To</label>
-                            <input
+                            <TextField
+                                fullWidth={true}
                                 className="Form-text-input"
-                                type="text" placeholder="Address"
+                                type="text"
+                                placeholder="Drop off address"
                                 value={this.state.toAddress}
                                 onChange={(event) => this.setState({ toAddress: event.target.value })} />
                         </div>
                         <div className="Form-row">
-                            <label className="Form-label">No. Passengers</label>
-                            <input
+                            <TextField
+                                floatingLabelText="Number of passengers"
+                                fullWidth={true}
                                 className="Form-num-input"
-                                type="number" min={1} max={5}
+                                type="number"
+                                min={1}
+                                max={5}
                                 value={this.state.numPassengers}
                                 onChange={(event) => this.setState({ numPassengers: event.target.value })} />
                         </div>
                         <div className="Form-row">
-                            <label className="Form-label">Accommodations</label>
-                            <input
+                            <TextField
+                                fullWidth={true}
                                 className="Form-text-input"
-                                type="text" placeholder="Baggage, etc."
+                                type="text"
+                                placeholder="Accomodations (baggage, etc.)"
                                 value={this.state.accommodations}
                                 onChange={(event) => this.setState({ accommodations: event.target.value })} />
                         </div>
-                        <button className="Request-button" onClick={this.requestRide.bind(this)}>Request Ride</button>
+                        <RaisedButton 
+                            className="Request-button"
+                            secondary={true}
+                            label="Request Ride"
+                            onClick={() => this.requestRide()} />
                     </div>
                 );
             case rideStatus.REQUESTED:
@@ -90,7 +102,9 @@ class HomeScreen extends Component {
                             <label className="Form-label">Accommodations:</label>
                             <label className="Ride-data">{this.state.accommodations}</label>
                         </div>
-                        <button className="Request-button" onClick={this.props.cancelRide}>Cancel Ride</button>
+                        <RaisedButton className="Request-button" onClick={this.props.cancelRide}>
+                            Cancel Ride
+                        </RaisedButton>
                     </div>
                 );
             default:
