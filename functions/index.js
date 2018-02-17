6b7cd66eaecf95664d1606491dc3367aa3dbeb61
Brian McCarthy <brianmcca1@gmail.com>
2018-02-16 22:44:57 -0500

    Basic structure for firebase functions

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,40 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// Validate a new ride
+exports.validateNewRide = functions.firestore.document('ApprovalQueue').onCreate(event => {
+	var newRide = event.data.data();
+
+	// See if there is currently a ride in an active queue with that user's email
+	// If so, reject the ride automatically.
+	var email = newRide.email;
+
+
+});
+
+exports.approvalUpdate = functions.firestore.document('ApprovalQueue').onUpdate(event => {
+	statusUpdate(event);
+});
+
+exports.waitingUpdate = functions.firestore.document('WaitingQueue').onUpdate(event => {
+	statusUpdate(event);
+})
+
+
+function statusUpdate(event){
+	var updatedRide = event.data.data();
+	var oldRide = event.data.previous.data();
+
+	var status = updatedRide.status;
+	var oldStatus = oldRide.status;
+
+	if(status != oldStatus){
+		// The status has been updated
+		if(status == "approved"){
+			// Move the ride to the WaitingQueue
+		} else if (status == "rejected" || status == "completed" || status == "cancelled" || status == "noShow"){
+			// move the ride to the Archive
+		}
+	}
+}
