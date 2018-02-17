0b4eaf55754700b44894f5e42a687be5f7613fb8
Brian McCarthy <brianmcca1@gmail.com>
2018-02-16 23:11:38 -0500

    Implemented moving rides between queues

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,20 @@
 const functions = require('firebase-functions');
 
+const admin = require('firebase-admin');
+
+var config = {
+	apiKey: "AIzaSyDb7pzdnEJJlClx4-enDHkTJR81yDg3HVs",
+	authDomain: "snapp-c0271.firebaseapp.com",
+	databaseURL: "https://snapp-c0271.firebaseio.com",
+	projectId: "snapp-c0271",
+	storageBucket: "snapp-c0271.appspot.com",
+	messagingSenderId: "702539875760"
+};
+
+firebase.initializeApp(config);
+
+var databse = admin.firebase;
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -14,6 +29,7 @@ exports.validateNewRide = functions.firestore.document('ApprovalQueue').onCreate
 	// See if there is currently a ride in an active queue with that user's email
 	// If so, reject the ride automatically.
 	var email = newRide.email;
+	
 
 
 });
@@ -38,8 +54,14 @@ function statusUpdate(event){
 		// The status has been updated
 		if(status == "approved"){
 			// Move the ride to the WaitingQueue
+			var data = updatedRide;
+			db.collection('WaitingQueue/').doc(updatedRide.id).set(data);
 		} else if (status == "rejected" || status == "completed" || status == "cancelled" || status == "noShow"){
 			// move the ride to the Archive
+			var data = updatedRide;
+			db.collection('Archive/').doc(updatedRide.id).set(data);
 		}
+		// Remove the old entry
+		event.data.ref.delete();
 	}
 }
