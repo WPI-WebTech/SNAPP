5983f04807679a4a4301ef762ed0f13465090cc4
Brian McCarthy <brianmcca1@gmail.com>
2018-02-21 23:39:43 -0500

    Email checking implementation

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,27 @@
 const functions = require('firebase-functions');
-
+const firebase = require('firebase');
 const admin = require('firebase-admin');
+var serviceAccount = {
+  "type": "service_account",
+  "project_id": "snapp-c0271",
+  "private_key_id": "57486c763b3851bcf6a86fc9357e772533c6ad42",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC6txLXMmj82wp1\n4nl9FLlG8igVVbw8gsajwVPnLFdLy9qJPNWHhfFD1tujD37oJLoTDmdOo0PvElc3\nUWWWmIgt5qfWnSHC0bKCthJJOq78rw2GL7sMc2cO/rqfeR/eLQsxk7MmyaQsKqqc\nuc1sGHC2ADYOxQDhTGE+ODb2uOAg80tTD6Js40KOpMiN0+lGiSelkH9y1PtZ6AKI\nrrmI5WtHxiTgUNGn/Cd7d3pgzHltVkMT0+z3CZLDv5K5ecPEDub2DsLQANEnrqmK\ng/DD4DRzGjJICPebVwUIwrGWrisuOsHnCOg/ywMqsfgSvKCiKorvOKP0bd0aKusq\nsVeHnQEbAgMBAAECggEACGhUbomebf4v6hQA7i+f70CT8Kpcsglj2W7XkNoPm7N0\nFwtIilzu+sD+b1SRKSBsbLw/KlsyUzsUDjdOIhLosixtwmwRvLPROWBe0ck9vDLP\nXPQn5hjDqdYljrLdEP57EQ3uyRqROsxyrRsqXyl5TROgW4Zab78PCtJU6/3PQXmY\nDf4c7nae+XXn8WsoJQ84IZadiWF9CrlGbZmOFmtLTZawS2PXTE+v7BMZS+KzxABy\ntYm1RyLBX+U0z0GjSBmzdphd1q67kB1cJAPJ/ZLaEiFpJSdV2G3a8b+A197KOvxx\nMzunN3YRhB9KZYLLTUrlHNrPTw7vLFOSoePd3uikMQKBgQDwPykHsBYx4QwZo29E\nP90Nv4Wu7i4XiWoP9auGrE4PIEhLv4NbxPLgipJ2aEZgVElWDx3CIY9vw+tVjqL0\nMmUYR4UlXD//XolqBPp6qtddk9tU6GSSgqLf6KRqII3Uc9pIzm5yTyKGaBUyvIXt\nJm/ZnzVZ4BX10qKmyR1nZUS/cQKBgQDG9VGv0Zm3FkX29sYX+ism7nuZ1s4OTI4L\nyB+/GPWDuNED2FwDF873LSLNwxAdQYUhNf8ASgmDFpn9LW8ulvPaloffvWaaW8gR\nyMYB9fZTXLq+8l4p8j+l9FSUelN272yzRxDxwqIBvdicNhqeWzuL8QiQG+66VzBY\nwjBLzMgbSwKBgDC47m5DtMya0mRks80IkqEWJVtrrGdTk/H2pbmjOhVxO5g5F7/F\numf1Umy3CwbAMTtFx4g2XCuTdSedzMhva/c3kfYYtlXo6z9YESBNg23T7ZhTpQnd\neoCkYquvPHRIpo07aETAbcUKWLo4Z4TH1ckxs5Utj+aRuDDQ+qicbgvRAoGAVNwz\n4EsFRltAskqlSXrRxwQ3QfGl5+FtQMc2CzspFOrSc9JnxIX2J+Vfj+XgTKoyj2zJ\nfCUYE4qjWaJ0ZqnKDvTftWXCipoVJsi88/8l9n55xJH56ehCj5U8MixOx07tzTXF\nm2iJ0zZXWqGVDvAY4OKI5XaUh3RC0cjZ8qrAuysCgYEA5Ek5of0Gmsr/qPynuAT8\nXK3AtoToS8dkbzx3XbFe7mC1LDWcK7L9rBPcQ1AEN++6ornZKcnrhqetVqbQNbvr\nNUMJjFBBWogvYQKO5PePupjN2rire9Gcbj7YLHT9wNjCIk+87JKLmAV5e1/VezzA\nzHD1+0SYB0xnr0O2NCO6udU=\n-----END PRIVATE KEY-----\n",
+  "client_email": "snapp-c0271@appspot.gserviceaccount.com",
+  "client_id": "115295893730525762252",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://accounts.google.com/o/oauth2/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/snapp-c0271%40appspot.gserviceaccount.com"
+}
+
 
 /**
  * Firebase functions to handle new rides and status changes for the SNAPP Firestore database
  *
  */
-var config = {
-	apiKey: "AIzaSyDb7pzdnEJJlClx4-enDHkTJR81yDg3HVs",
-	authDomain: "snapp-c0271.firebaseapp.com",
-	databaseURL: "https://snapp-c0271.firebaseio.com",
-	projectId: "snapp-c0271",
-	storageBucket: "snapp-c0271.appspot.com",
-	messagingSenderId: "702539875760"
-};
-
-firebase.initializeApp(config);
-
-var databse = admin.firebase;
 
 // Validate a new ride
-exports.validateNewRide = functions.firestore.document('ApprovalQueue').onCreate(event => {
+exports.validateNewRide = functions.firestore.document('ApprovalQueue/{rideID}').onCreate(event => {
 	var newRide = event.data.data();
 
 	// See if there is currently a ride in an active queue with that user's email
@@ -37,17 +38,27 @@ exports.validateNewRide = functions.firestore.document('ApprovalQueue').onCreate
 });
 
 // Handle a change in a document in the ApprovalQueue
-exports.approvalUpdate = functions.firestore.document('ApprovalQueue').onUpdate(event => {
+exports.approvalUpdate = functions.firestore.document('ApprovalQueue/{rideID}').onUpdate(event => {
 	statusUpdate(event);
 });
 
 // Handle a change in a document in the WaitingQueue
-exports.waitingUpdate = functions.firestore.document('WaitingQueue').onUpdate(event => {
+exports.waitingUpdate = functions.firestore.document('WaitingQueue/{rideID}').onUpdate(event => {
 	statusUpdate(event);
 })
 
+
+function initializeFirebase(){
+	admin.initializeApp({
+		credential : admin.credential.cert(serviceAccount)
+	});
+
+	return admin.firestore();
+
+}
 function containsEmail(tableName, email){
-	var docRef = db.collecton(tableName);
+	database = initializeFirebase();
+	var docRef = database.collecton(tableName);
 	var allRides = docRef.get().then(snapshot => {
 		var docEmail = snapshot.data().email;
 		if(docEmail == email){
@@ -60,6 +71,7 @@ function containsEmail(tableName, email){
  * Handle a change in a document in the ApprovalQueue or the WaitingQueue
  */
 function statusUpdate(event){
+	database = initializeFirebase();
 	var updatedRide = event.data.data();
 	var oldRide = event.data.previous.data();
 
@@ -71,11 +83,12 @@ function statusUpdate(event){
 		if(status == "approved"){
 			// Move the ride to the WaitingQueue
 			var data = updatedRide;
-			db.collection('WaitingQueue/').doc(updatedRide.id).set(data);
+			console.log("Updated ride's ID is " + updatedRide.id);
+			database.collection('WaitingQueue/').doc(updatedRide.id).set(data);
 		} else if (status == "rejected" || status == "completed" || status == "cancelled" || status == "noShow"){
 			// move the ride to the Archive
 			var data = updatedRide;
-			db.collection('Archive/').doc(updatedRide.id).set(data);
+			database.collection('Archive/').doc(updatedRide.id).set(data);
 		}
 		// Remove the old entry
 		event.data.ref.delete();
